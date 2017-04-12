9da94ebcf11863fd9f6e5d1bf531883e00e1eb4b
Konstantin Kelemen <konstantin@kel.mn>
2017-04-12 09:22:43 +0300

    Improve the Write_amiibo sketch. Add dump example and halt the write process while it's not too late.

--- Write_amiibo/Write_amiibo.ino
+++ Write_amiibo/Write_amiibo.ino
@@ -46,6 +46,20 @@ void loop() {
 
   //    >>> Paste your dump here <<<
 
+/*  This is an example of how the pasted dump should look like:
+
+    0x04, 0x4C, 0x4F, 0x8F,
+    0x2A, 0xE3, 0x3E, 0x81,
+    0x76, 0x48, 0x0F, 0xE0,
+    0xF1, 0x10, 0xFF, 0xEE,
+    ...
+    0x01, 0x00, 0x0F, 0xBD,
+    0x00, 0x00, 0x00, 0x04,
+    0x5F, 0x00, 0x00, 0x00,
+    0xCC, 0xF9, 0xBE, 0x37,
+    0x80, 0x80, 0x00, 0x00   <-- Notice the absence of the coma in the end of the last line.
+
+*/
   };
 
   MFRC522::StatusCode status;
@@ -60,6 +74,19 @@ void loop() {
     if (status != MFRC522::STATUS_OK) {
       Serial.print(F("MIFARE_Write() failed: "));
       Serial.println(mfrc522.GetStatusCodeName(status));
+      Serial.println("Write process failed, please try once more.");
+      Serial.println("Your tag is still fine, just remove it and put back again in 3 seconds.");
+      delay(1000);
+      Serial.println();
+      Serial.print("New attempt in 3...");
+      delay(1000);
+      Serial.print("2...");
+      delay(1000);
+      Serial.print("1...");
+      delay(1000);
+      Serial.println();
+      Serial.println();
+      Serial.println("Ready to write.");
     }
     Serial.println();
   }
@@ -99,7 +126,7 @@ void loop() {
   // Stop encryption on PCD
   mfrc522.PCD_StopCrypto1();
 
-  Serial.println("Write finished! Now please take your Amiibo card away!");
+  Serial.println("Write process finished! Now please take your Amiibo card away!");
   delay(30000);
 }
 
